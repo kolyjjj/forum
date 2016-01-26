969006618368776dbf2b3b4f0646a53f3a2cd3f6
kolyjjj <me.koly.li@gmail.com>
2016-01-26 16:39:37 +0800

    use nodemon to watch the changes and rerun the app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import express from 'express';
 let app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('Hello ExpressJS! It is slow, but it is working...');
 });
 
 app.listen(3000, () => {
--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,12 +2,26 @@
 
 import gulp from 'gulp';
 import babel from 'gulp-babel';
+import nodemon from 'gulp-nodemon';
+import Cache from 'gulp-file-cache';
+
+const cache = new Cache();
 
 
-gulp.task('build',  () => {
+gulp.task('compile',  () => {
   return gulp.src(['app.js', 'src/**/*.js'])
+        .pipe(cache.filter())
         .pipe(babel({
          presets: ['es2015']
         }))
+        .pipe(cache.cache())
         .pipe(gulp.dest('dist'));
+    });
+
+gulp.task('start', ['compile'], () => {
+    return nodemon({
+        script: 'dist/app.js',
+        watch: ['src/**/*.js', 'app.js'],
+        tasks: ['compile']
+        });
     });
