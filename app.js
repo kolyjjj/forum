e75d9670e861cd7b12dfd288adee0957d0e026e4
kolyjjj <me.koly.li@gmail.com>
2016-01-26 17:56:27 +0800

    add routing for 404

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ let app = express();
 
 app.use('/api', routers);
 
+app.get('*', (req, res) => {
+    res.status(404).send('What????');
+    });
+
 app.listen(3000, () => {
    console.log('Example app listening on port 3000!');
     });
