f64910b58e139862bff5327a52b647efa2dd79bd
kolyjjj <me.koly.li@gmail.com>
2016-01-27 15:21:05 +0800

    use mongoose to connect with mongodb

---
+++ src/posts/postsdb.js
@@ -0,0 +1,46 @@
+import mongoose from 'mongoose';
+
+const dburl = 'mongodb://192.168.99.100/test';
+mongoose.connect(dburl);
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+    console.log('connected to mongodb');
+});
+
+const Schema = mongoose.Schema;
+const blogSchema = new Schema({
+    title: String, 
+      author: String, 
+      body: String,
+      comments: [{body: String, date: Date}],
+      date: {type: Date, default: Date.now},
+      hidden: Boolean,
+      meta: {
+          votes: Number,
+      favs: Number
+      }
+});
+
+const Blog = mongoose.model('Blog', blogSchema);
+let aBlog = new Blog({
+    title: 'first blog',
+    author: 'koly',
+    body: 'What a beautiful world',
+    comments: [{body: 'a comment', date: Date.now()}],
+    hidden: false,
+    meta: {
+        votes: 1,
+    favs: 1
+    }
+});
+
+console.log(aBlog.title + '->' + aBlog.author + '->' + aBlog.body);
+
+const postsdb = {
+    save() {
+        return aBlog.save();    }
+};
+
+export default postsdb;
--- src/posts/router.js
+++ src/posts/router.js
@@ -1,9 +1,24 @@
 import express from 'express';
+import postsdb from './postsdb';
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.status(200).send('posts');
+    //postsdb.getAllPosts().then((data)=>{
+    //    res.status(200).json(data);
+    //    });
+    //res.status(200).send('posts');
+});
+
+router.get('/save', (req, res) => {
+    postsdb.save().then((data) => {
+        console.log('succeed');
+        console.log(data);
+        res.status(200).json(data);
+        }, (err)=>{
+            res.status(500).json(err);
+            });
+    //res.status(200).json();
     });
 
 export default router;
