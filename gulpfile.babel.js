7a889b3f9462db68385facb3adb6d01a3e9a5da1
kolyjjj <me.koly.li@gmail.com>
2016-02-14 14:40:04 +0800

    use variable value directly

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -8,36 +8,36 @@ import minimist from 'minimist';
 
 const cache = new Cache();
 const knownOptions = {
-    string: 'env',
-    default: { env: process.env.NODE_ENV || 'local' }
+  string: 'env',
+  default: { env: process.env.NODE_ENV || 'local' }
 };
 
 const options = minimist(process.argv.slice(2), knownOptions);
 const environmentPath = `env/${options.env}/*.js`;
 
 gulp.task('env', () => {
-    return gulp.src(`${environmentPath}`)
-          .pipe(cache.filter())
-          .pipe(babel())
-          .pipe(cache.cache())
-          .pipe(gulp.dest('dist/env'));
-    });
+  return gulp.src(environmentPath)
+  .pipe(cache.filter())
+  .pipe(babel())
+  .pipe(cache.cache())
+  .pipe(gulp.dest('dist/env'));
+});
 
 gulp.task('compile', ['env'], () => {
 
   return gulp.src(['app.js', 'src/**/*.js'], {base: '.'})
-        .pipe(cache.filter())
-        .pipe(babel({
-         presets: ['es2015']
-        }))
-        .pipe(cache.cache())
-        .pipe(gulp.dest('dist'));
-    });
+  .pipe(cache.filter())
+  .pipe(babel({
+    presets: ['es2015']
+  }))
+  .pipe(cache.cache())
+  .pipe(gulp.dest('dist'));
+});
 
 gulp.task('start', ['compile'], () => {
-    return nodemon({
-        script: 'dist/app.js',
-        watch: ['src/**/*.js', 'app.js'],
-        tasks: ['compile']
-        });
-    });
+  return nodemon({
+    script: 'dist/app.js',
+    watch: ['src/**/*.js', 'app.js'],
+    tasks: ['compile']
+  });
+});
