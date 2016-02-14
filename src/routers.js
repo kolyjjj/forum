3abfb705b4f23e0c5a4be287a6e90041884462a6
kolyjjj <me.koly.li@gmail.com>
2016-02-14 14:31:53 +0800

    identation change

--- src/routers.js
+++ src/routers.js
@@ -5,32 +5,32 @@ import postRouter from './posts/router';
 const router = express.Router();
 
 router.use(function timeLog(req, res, next){
-    console.log('Time: ', Date.now());
-    next();
+  console.log('Time: ', Date.now());
+  next();
 });
 
 router.all('*', function onlyAllowJson(req, res, next) {
-    const method = req.method;
-    console.log(method, typeof method);
-    if (lodash.includes(['GET'], method)) next();
-    else {
-        const contentType = req.get('Content-Type');
-        if (contentType && contentType.includes('application/json')) 
-    next();
-        else 
-    res.status(400).send('wrong Content-Type, should be Content-Type:application/json, yours is ' + contentType);
-    }
+  const method = req.method;
+  console.log(method, typeof method);
+  if (lodash.includes(['GET'], method)) next();
+  else {
+    const contentType = req.get('Content-Type');
+    if (contentType && contentType.includes('application/json')) 
+      next();
+    else 
+      res.status(400).send('wrong Content-Type, should be Content-Type:application/json, yours is ' + contentType);
+  }
 });
 
 router.post('*', function postShouldHasContent(req, res, next) {
-    if (req.body && !lodash.isEmpty(req.body)) 
+  if (req.body && !lodash.isEmpty(req.body)) 
     next();
-    else
+  else
     res.status(400).send('post should contain valid body');
 });
 
 router.get('/', (req, res) => {
-    res.send('hello, here is the api');
+  res.send('hello, here is the api');
 });
 
 router.use('/posts/', postRouter); 
