7deeebf28612a34bbcff61616bf3fa19927f561d
kolyjjj <me.koly.li@gmail.com>
2016-02-16 15:54:17 +0800

    add test for getting one post

--- test/posts/api.spec.js
+++ test/posts/api.spec.js
@@ -3,11 +3,41 @@
 import request from 'supertest';
 import app from '../../app';
 
-describe('GET /posts', ()=>{
+describe('/api/posts', ()=>{
+  let aPost = {
+    "title":"A new Post B",
+    "author":"koly",
+    "content":"Hello this is a post.",
+    "comments":[],
+    "hidden": false,
+    "meta": {}
+  };
+
+  it('should create one post', function(done){
+    request(app)
+    .post('/api/posts')
+    .send(aPost)
+    .expect('Content-Type', /json/)
+    .expect((res)=>{
+      console.log('body', res.body);
+    })
+    .expect(200, done);
+  });
+
   it('should get one post', function(done){
     request(app)
-      .get('/api/posts')
+    .post('/api/posts')
+    .send(aPost)
+    .expect('Content-Type', /json/)
+    .expect(200)
+    .end((err, res)=>{
+      request(app)
+      .get('/api/posts/' + res.body.id)
       .expect('Content-Type', /json/)
+      .expect((res)=>{
+        console.log('getting response', res.body);
+      })
       .expect(200, done);
+    });
   });
 });
