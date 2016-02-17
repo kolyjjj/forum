ef3038ff450175b5713c5eee8a1aaef3b31b2320
kolyjjj <me.koly.li@gmail.com>
2016-02-17 15:28:16 +0800

    add more tests

--- test/posts/api.spec.js
+++ test/posts/api.spec.js
@@ -1,6 +1,7 @@
 'use strict';
 
 import request from 'supertest';
+import should from 'should';
 import app from '../../app';
 
 describe('/api/posts', ()=>{
@@ -13,13 +14,35 @@ describe('/api/posts', ()=>{
     "meta": {}
   };
 
+  //after(function(done)=>{
+    //request(app)
+    //.get('/api/posts')
+    //.expect('Content-Type', /json/)
+    //.end((err, res)=>{
+      //if (err) throw err;
+
+    //})
+  //})
+
+  it('should get posts', function(done){
+    request(app)
+    .get('/api/posts')
+    .expect('Content-Type', /json/)
+    .expect((res)=>{
+      console.log('getting posts');
+      res.body.should.be.instanceof(Array);
+    })
+    .expect(200, done); // done should be here, if put done one line above and call it, it doesn't work
+  });
+
   it('should create one post', function(done){
     request(app)
     .post('/api/posts')
     .send(aPost)
     .expect('Content-Type', /json/)
     .expect((res)=>{
-      console.log('body', res.body);
+      console.log('creating one post');
+      res.body.should.have.property('id');
     })
     .expect(200, done);
   });
@@ -31,12 +54,32 @@ describe('/api/posts', ()=>{
     .expect('Content-Type', /json/)
     .expect(200)
     .end((err, res)=>{
+      if (err) throw err;
       request(app)
       .get('/api/posts/' + res.body.id)
       .expect('Content-Type', /json/)
       .expect((res)=>{
-        console.log('getting response', res.body);
+        console.log('getting a post');
+        let body = res.body;
+        body.title.should.be.exactly(aPost.title);
+        body.author.should.be.exactly(aPost.author);
+        body.content.should.be.exactly(aPost.content);
       })
+      .expect(200, done);
+    });
+  });
+
+  it('should delete a post', function(done){
+    request(app)
+    .post('/api/posts')
+    .send(aPost)
+    .expect('Content-Type', /json/)
+    .expect(200)
+    .end((err, res)=>{
+      if (err) throw err;
+      console.log('deleting');
+      request(app)
+      .delete('/api/posts/'+res.body.id)
       .expect(200, done);
     });
   });
