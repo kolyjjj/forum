1879174f58221d9bcfaa9b6287ee49393611da23
kolyjjj <me.koly.li@gmail.com>
2016-03-22 23:43:38 +0800

    user user defined formatter

--- src/logger/index.js
+++ src/logger/index.js
@@ -5,7 +5,16 @@ import winston from 'winston';
 const logger = new (winston.Logger)({
   level: 'debug',
   transports: [
-    new (winston.transports.Console)(),
+    new (winston.transports.Console)({
+      timestamp: function() {
+        return Date.now();
+      },
+      formatter: function(options) {
+        // Return string will be passed to logger.
+        return options.timestamp() +' '+ options.level.toUpperCase() +' '+ (undefined !== options.message ? options.message : '') +
+          (options.meta && Object.keys(options.meta).length ? '\n\t'+ JSON.stringify(options.meta) : '' );
+      }
+    }),
     new (winston.transports.File)({ filename: 'app.log' })
   ]}
   );
