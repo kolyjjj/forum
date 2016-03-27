1e70e0a678e99799398a7c2a626caa052480253c
kolyjjj <me.koly.li@gmail.com>
2016-03-27 10:10:33 +0800

    use lodash some method

--- src/auth/service.js
+++ src/auth/service.js
@@ -20,21 +20,10 @@ const authService = {
     this.excludedAction = parseActions(actions);
   },
   isExcludedAction(verb, url){
-    console.log('excluded action', this.excludedAction);
-    for (let elem of this.excludedAction) {
-      console.log('=====', verb, url, elem.method, lodash.isEmpty(elem.method), elem.urlRegex, new RegExp('^' + elem.urlRegex + '$').test(url));
-      const matchUrl = new RegExp('^' + elem.urlRegex + '$').test(url);
-      if(lodash.isEmpty(elem.method) ? matchUrl : (elem.method.toUpperCase() === verb.toUpperCase() && matchUrl)){
-          return true;
-        }
-    }
-    return false;
+    return lodash.some(this.excludedAction, function(e) {
+      const matchUrl = new RegExp('^' + e.urlRegex + '$').test(url);
+      return lodash.isEmpty(e.method) ? matchUrl : (e.method.toUpperCase() === verb.toUpperCase() && matchUrl)});
   }
-  //return lodash.some(this.excludedAction, function(e) {
-  //console.log('=====', verb, url, lodash.isEmpty(e.method), e.urlRegex, new RegExp('^' + e.urlRegex + '$').test(url));
-  //const matchUrl = new RegExp('^' + e.urlRegex + '$').test(url);
-  //return lodash.isEmpty(e.method) ? matchUrl : (e.method.toUpperCase() === verb.toUpperCase() && matchUrl)});
-  //}
 };
 
 export default authService;
